77dd48711a597fe6f418a80180a176bc0931bd5a
Paulo Duarte <p.roger.antunes43@gmail.com>
2015-10-28 13:14:44 -0200

    Initial commit.

--- PIM IV/src/PIM IV.c
+++ PIM IV/src/PIM IV.c
@@ -10,8 +10,65 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define menuSize 3
+
+void waitingForUser(void) {
+	char prev = 0;
+	printf("Precione [Enter] para continuar.\n");
+	while (1) {
+		char c = getchar();
+		if (c == '\n' && prev == c) {
+			break;
+		}
+		prev = c;
+	}
+}
 
 int main(void) {
-	puts("!!!PIM IV!!!"); /* prints !!!PIM IV!!! */
-	return EXIT_SUCCESS;
+	char bus[9][4];
+	char *cities[] = { "Porto Alegre", "Canoas", "Esteio", "Sapucaia do Sul",
+			"São Leopoldo", "Novo Hamburgo" };
+	int num_states = 6;
+	int i;
+	int j;
+	int option;
+	int start;
+	int end;
+
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 4; j++) {
+			bus[i][j] = '0';
+		}
+	}
+
+	while (1) {
+		READMENU: printf("\nNOME DA EMPRESA DE ONIBUS\n\n");
+		printf("1 - Vender passagem\n");
+		printf("2 - Relatório de vendas\n");
+		printf("0 - Sair do sistema\n\n");
+		printf("Selecione uma opção: ");
+		scanf("%d", &option);
+
+		if (option < 0 || option > menuSize) {
+			printf("Opção inválida\n");
+			goto READMENU;
+		}
+
+		switch (option) {
+		case 0:
+			printf("Até mais!");
+			return EXIT_SUCCESS;
+		case 1:
+			printf("Venda de passagens\n\n");
+			for (i = 0; i < num_states; i++) {
+				printf("%d: %s\n", i, cities[i]);
+			}
+			break;
+		case 2:
+			printf("Relatório de vendas\n\n");
+			break;
+		}
+		waitingForUser();
+	}
 }
+
