6ee48a7607b97e14a318cc2779f9e17d2e89362c
Paulo Duarte <p.roger.antunes43@gmail.com>
2015-10-28 11:15:11 -0200

    commit inicial

---
+++ PIM IV/src/PIM IV.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : PIM.c
+ Author      : Paulo Duarte
+ Version     :
+ Copyright   : © 2015 Paulo Duarte All Rights Reserved
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!PIM IV!!!"); /* prints !!!PIM IV!!! */
+	return EXIT_SUCCESS;
+}
